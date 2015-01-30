3ae8a4e14e70ea4622d86cee36fafb2b485fe7d7
Paul Andrieux <paul@appventus.com>
2015-01-30 17:51:28 +0100

    create a user interface

---
+++ Entity/UserInterface.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppVentus\MangopayBundle\Entity;
+
+/**
+ * Defines mandatory methods a Mango user should have
+ * https://docs.mangopay.com/api-references/users/natural-users/
+ */
+interface UserInterface
+{
+    /**
+     * @var integer
+     */
+    public function getMangoUserId();
+
+    /**
+     * @var string
+     * User’s e-mail. A correct email address is expected
+     */
+    public function getEmail();
+
+    /**
+     * @var string
+     * User’s firstname (<100 chars)
+     */
+    public function getFirstname();
+
+    /**
+     * @var string
+     * User’s lastname (<100 chars)
+     */
+    public function getLastname();
+
+    /**
+     * @var date
+     * User’s birthdate. A Timestamp is expected
+     */
+    public function getBirthDate();
+
+    /**
+     * @var string
+     * User’s Nationality. ISO 3166-1 alpha-2 format is expected
+     */
+    public function getNationality();
+
+    /**
+     * @var string
+     * User’s country of residence. ISO 3166-1 alpha-2 format is expected
+     */
+    public function getCountry();
+
+}
