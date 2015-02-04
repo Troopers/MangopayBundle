f38149180523f4f5eab2d2dd59832b27c15fa6c7
Paul Andrieux <paul@appventus.com>
2015-02-04 15:03:27 +0100

    create a UserHelper

--- Helper/MangopayHelper.php
+++ Helper/MangopayHelper.php
@@ -1,17 +1,17 @@
 <?php
+
 namespace AppVentus\MangopayBundle\Helper;
 
-use AppVentus\MangopayBundle\Entity\UserInterface;
-use MangoPay\CardRegistration;
+use Doctrine\ORM\EntityManager;
 use MangoPay\MangoPayApi;
-use MangoPay\UserNatural;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
  * This class is an interface between raw MongoPay api object and symfony2.
  * It is declared as a service with application wide "clientId", "clientPassword" and "baseUrl" parameters
  * This service provides some shortcuts to interact with the api
  *
- * You can use it threw "appventus_mangopay.mango_api" service.
+ * ref: appventus_mangopay.mango_api
  *
  **/
 class MangopayHelper extends MangoPayApi
@@ -20,55 +20,17 @@ class MangopayHelper extends MangoPayApi
     protected $clientPassword;
     protected $baseUrl;
     protected $dispatcher;
+    protected $entityManager;
 
-    public function __construct($clientId, $clientPassword, $baseUrl, $dispatcher, $debug = false)
+    public function __construct($clientId, $clientPassword, $baseUrl, EventDispatcherInterface $dispatcher, EntityManager $entityManager)
     {
-        parent::__construct($debug);
+        parent::__construct();
         $this->Config->ClientId = $clientId;
         $this->Config->ClientPassword = $clientPassword;
         $this->Config->TemporaryFolder = sys_get_temp_dir();
         $this->Config->BaseUrl = $baseUrl;
         $this->dispatcher = $dispatcher;
+        $this->entityManager = $entityManager;
     }
 
-    /**
-     * post given CardRegistration to mango, and return populated CardRegistration
-     *
-     * @param  CardRegistration $card
-     * @return $card
-     **/
-    public function createCardRegistration(CardRegistration $card)
-    {
-        $card->Currency = "EUR";
-        $card = $this->CardRegistrations->create($card);
-
-        return $card;
-    }
-
-    public function createMangoUser(UserInterface $user)
-    {
-        $mangoUser = new UserNatural();
-        $mangoUser->Email = $user->getEmail();
-        $mangoUser->FirstName = $user->getFirstname();
-        $mangoUser->LastName = $user->getLastname();
-        $mangoUser->Birthday = $user->getBirthDate();
-        $mangoUser->Nationality = $user->getNationality();
-        $mangoUser->CountryOfResidence = $user->getCountry();
-
-        $mangoUser = $this->Users->Create($mangoUser);
-
-        return $mangoUser;
-    }
-
-    public function findOrCreateMangoUser(UserInterface $user)
-    {
-        if ($user->getMangoUserId()) {
-            $mangoUser = $this->Users->get($user->getMangoUserId());
-        // else, create a new mango user
-        } else {
-            $mangoUser = $this->createMangoUser($user);
-        }
-
-        return $mangoUser;
-    }
 }
---
+++ Helper/UserHelper.php
@@ -0,0 +1,65 @@
+<?php
+namespace AppVentus\MangopayBundle\Helper;
+
+use AppVentus\MangopayBundle\AppVentusMangopayEvents;
+use AppVentus\MangopayBundle\Entity\UserInterface;
+use AppVentus\MangopayBundle\Event\UserEvent;
+use Doctrine\ORM\EntityManager;
+use MangoPay\User;
+use MangoPay\UserNatural;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+/**
+ *
+ * ref: appventus_mangopay.user_helper
+ *
+ **/
+class UserHelper
+{
+    private $mangopayHelper;
+    private $entityManager;
+    private $dispatcher;
+
+    public function __construct(MangopayHelper $mangopayHelper, EntityManager $entityManager, EventDispatcherInterface $dispatcher)
+    {
+        $this->mangopayHelper = $mangopayHelper;
+        $this->entityManager = $entityManager;
+        $this->dispatcher = $dispatcher;
+    }
+
+    public function findOrCreateMangoUser(UserInterface $user)
+    {
+        if ($mangoUserId = $user->getMangoUserId()) {
+            $mangoUser = $this->mangopayHelper->Users->get($mangoUserId);
+        } else {
+            $mangoUser = $this->createMangoUser($user);
+        }
+
+        return $mangoUser;
+    }
+    public function createMangoUser(UserInterface $user)
+    {
+
+        $mangoUser = new UserNatural();
+        $mangoUser->Email = $user->getEmail();
+        $mangoUser->FirstName = $user->getFirstname();
+        $mangoUser->LastName = $user->getLastname();
+        $mangoUser->Birthday = $user->getBirthDate();
+        $mangoUser->Nationality = $user->getNationality();
+        $mangoUser->CountryOfResidence = $user->getCountry();
+        $mangoUser->Tag = $user->getId();
+
+        $mangoUser = $this->mangopayHelper->Users->Create($mangoUser);
+
+        $event = new UserEvent($user, $mangoUser);
+        $this->dispatcher->dispatch(AppVentusMangopayEvents::NEW_USER, $event);
+
+        $user->setMangoUserId($mangoUser->Id);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return $mangoUser;
+    }
+
+}
