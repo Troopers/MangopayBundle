3f93a603d54b7093c759132db17f2dcfb53db180
Paul Andrieux <paul@appventus.com>
2015-05-06 14:12:53 +0200

    add validation group for card form

--- Form/CardType.php
+++ Form/CardType.php
@@ -17,7 +17,7 @@ class CardType extends AbstractType
     {
         $builder
             ->add('cardNumber', 'text', array(
-                'constraints' => array(new NotBlank()),
+                    'constraints' => array(new NotBlank(['groups' => ['card']])),
                 'label' => 'appventus_mangopay.card_number.label',
                 'attr' => array(
                     'data-id' => 'appventus_mangopay_card_number',
@@ -26,7 +26,7 @@ class CardType extends AbstractType
                 'mapped' => false
             ))
             ->add('cardHolder', 'text', array(
-                'constraints' => array(new NotBlank()),
+                    'constraints' => array(new NotBlank(['groups' => ['card']])),
                 'label' => 'appventus_mangopay.card_holder.label',
                 'attr' => array(
                     'data-id' => 'appventus_mangopay_card_holder',
@@ -35,7 +35,7 @@ class CardType extends AbstractType
                 'mapped' => false
             ))
             ->add('ccv', 'integer', array(
-                'constraints' => array(new NotBlank()),
+                    'constraints' => array(new NotBlank(['groups' => ['card']])),
                 'label' => 'appventus_mangopay.card_ccv.label',
                 'attr' => array(
                     'data-id' => 'appventus_mangopay_ccv',
@@ -44,7 +44,7 @@ class CardType extends AbstractType
                 'mapped' => false
             ))
             ->add('cardExpiryMonth', 'choice', array(
-                'constraints' => array(new NotBlank()),
+                    'constraints' => array(new NotBlank(['groups' => ['card']])),
                 'label' => 'appventus_mangopay.card_expiry_month.label',
                 'choices' => array(
                     "01" => "01",
@@ -76,7 +76,7 @@ class CardType extends AbstractType
             }
 
             $builder->add('cardExpiryYear', 'choice', array(
-                'constraints' => array(new NotBlank()),
+                    'constraints' => array(new NotBlank(['groups' => ['card']])),
                 'choices' => $years,
                 'attr' => array(
                     'data-id' => 'appventus_mangopay_card_expiry_year',
