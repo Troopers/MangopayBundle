c270b67d040ea310ed6d00f2ade54bce5e9cf1ff
Paul Andrieux <paul@appventus.com>
2015-08-18 15:15:15 +0200

    add Order events

---
+++ Event/OrderEvent.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppVentus\MangopayBundle\Event;
+
+use AppVentus\MangopayBundle\Entity\Order;
+use Symfony\Component\EventDispatcher\Event;
+;
+
+class OrderEvent extends Event
+{
+    protected $order;
+
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    public function getOrder()
+    {
+        return $this->order;
+    }
+
+    public function setOrder(Order $order)
+    {
+        $this->order = $order;
+        return $this;
+    }
+}
---
+++ OrderEvents.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppVentus\MangopayBundle;
+
+final class OrderEvents
+{
+    /**
+     * Event order.created occur when an order is created
+     *
+     * @var string
+     */
+    const ORDER_CREATED = 'order.created';
+
+}
