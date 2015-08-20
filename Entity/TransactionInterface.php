2b879697b7f43b6cacca6c63c65a612cd6bdd7ef
Zadkiel Aslafy-Aharonian <aslafy_z@epitech.eu>
2015-08-20 13:44:19 +0200

    PaymentDirect: updated the TransactionInterface tomake it  work with the Transaction object

--- Entity/TransactionInterface.php
+++ Entity/TransactionInterface.php
@@ -9,36 +9,36 @@ namespace AppVentus\MangopayBundle\Entity;
 interface TransactionInterface
 {
     /**
-     * Author Mango Id
+     * Author Id
      * @var string
      */
-    public function getAuthorMangoId();
-    
+    public function getAuthorId();
+
     /**
-     * It represents the amount debited on the bank account of the Author.In cents so 100€ will be written like « Amount » : 10000
+     * It represents the amount debited on the bank account of the Author. In cents so 100€ will be written like « Amount » : 10000
      * DebitedFunds – Fees = CreditedFunds (amount received on wallet)
-     * 
+     *
      * @var string
      */
     public function getDebitedFunds();
-    
+
     /**
      * It represents your fees taken on the DebitedFunds.In cents so 100€ will be written like « Amount » : 10000
-     * 
+     *
      * @var integer
      */
     public function getFees();
-    
+
     /**
      * The Mango ID of the credited wallet
-     * 
+     *
      * @var integer
      */
     public function getCreditedWalletId();
-    
+
     /**
      * URL Format expected
-     * 
+     *
      * @var integer
      */
     public function getCardType();
