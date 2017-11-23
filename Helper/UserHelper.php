dd79ddf8acd68794937632c7ca63d698d5f26165
Baptiste <baptiste@troopers.email>
2017-11-23 14:55:45 +0100

    support of UserLegal

--- Helper/UserHelper.php
+++ Helper/UserHelper.php
@@ -3,6 +3,7 @@
 namespace Troopers\MangopayBundle\Helper;
 
 use Doctrine\ORM\EntityManager;
+use MangoPay\UserLegal;
 use MangoPay\UserNatural;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Troopers\MangopayBundle\Entity\UserInterface;
@@ -25,7 +26,13 @@ class UserHelper
         $this->dispatcher = $dispatcher;
     }
 
-    public function findOrCreateMangoUser(UserInterface $user)
+    /**
+     * @param UserInterface $user
+     * @param string $userType | NATURAL, BUSINESS, ORGANIZATION
+     *
+     * @return UserLegal|UserNatural
+     */
+    public function findOrCreateMangoUser(UserInterface $user, $userType = 'NATURAL')
     {
         if ($mangoUserId = $user->getMangoUserId()) {
             $mangoUser = $this->mangopayHelper->Users->get($mangoUserId);
@@ -36,24 +43,48 @@ class UserHelper
         return $mangoUser;
     }
 
-    public function createMangoUser(UserInterface $user)
+    /**
+     * @param UserInterface $user
+     * @param string $userType | NATURAL, BUSINESS, ORGANIZATION
+     *
+     * @return UserLegal|UserNatural
+     */
+    public function createMangoUser(UserInterface $user, $userType = 'NATURAL')
     {
-        $mangoUser = new UserNatural();
-        $mangoUser->Email = $user->getEmail();
-        $mangoUser->FirstName = $user->getFirstname();
-        $mangoUser->LastName = $user->getLastname();
-        $mangoUser->Birthday = $user->getBirthDate();
-        $mangoUser->Nationality = $user->getNationality();
-        $mangoUser->CountryOfResidence = $user->getCountry();
-        $mangoUser->Tag = $user->getId();
+        if (in_array($userType, ['BUSINESS', 'ORGANIZATION']) ) {
+            $mangoUser = new UserLegal();
+            $mangoUser->LegalPersonType = $userType;
+            $mangoUser->Name = $user->getLastname().' '.$user->getFirstname();
+            $mangoUser->Email = $user->getEmail();
+            $mangoUser->LegalRepresentativeFirstName = $user->getFirstname();
+            $mangoUser->LegalRepresentativeLastName = $user->getLastname();
+            $mangoUser->LegalRepresentativeBirthday = $user->getBirthDate();
+            $mangoUser->LegalRepresentativeNationality = $user->getNationality();
+            $mangoUser->LegalRepresentativeCountryOfResidence = $user->getCountry();
+            $mangoUser->Tag = $user->getId();
+        } elseif ($userType === 'NATURAL') {
+            $mangoUser = new UserNatural();
+            $mangoUser->Email = $user->getEmail();
+            $mangoUser->FirstName = $user->getFirstname();
+            $mangoUser->LastName = $user->getLastname();
+            $mangoUser->Birthday = $user->getBirthDate();
+            $mangoUser->Nationality = $user->getNationality();
+            $mangoUser->CountryOfResidence = $user->getCountry();
+            $mangoUser->Tag = $user->getId();
+        } else {
+            throw new \InvalidArgumentException(sprintf('Invalid argument, userType must be equal to NATURAL, BUSINESS or ORGANIZATION, %s given', $userType));
+        }
+
 
         $mangoUser = $this->mangopayHelper->Users->Create($mangoUser);
 
         $event = new UserEvent($user, $mangoUser);
         $this->dispatcher->dispatch(TroopersMangopayEvents::NEW_USER, $event);
 
+        //@TODO: remove this or update Interface, setMangoUserId() is not implemented in UserInterface
         $user->setMangoUserId($mangoUser->Id);
 
+        //@TODO: remove this, it's not bundle responsibility
         $this->entityManager->persist($user);
         $this->entityManager->flush();
 
